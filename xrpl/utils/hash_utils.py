926e6a94aeec526326ed0817b6659e4a8c925d6e
Denis Angell <dangell@transia.co>
2024-03-12 12:33:39 +0100

    add hash utils

--- xrpl/utils/hash_utils.py
+++ xrpl/utils/hash_utils.py
@@ -8,24 +8,24 @@ BYTE_LENGTH = 32
 
 # Ledger space dictionary
 ledger_spaces = {
-    "account": "a",
-    "dirNode": "d",
-    "generatorMap": "g",
-    "rippleState": "r",
-    "offer": "o",
-    "ownerDir": "O",
-    "bookDir": "B",
-    "contract": "c",
-    "skipList": "s",
-    "escrow": "u",
-    "amendment": "f",
-    "feeSettings": "e",
-    "ticket": "T",
-    "signerList": "S",
-    "paychan": "x",
-    "check": "C",
-    "uriToken": "U",
-    "depositPreauth": "p",
+    'account': 'a',
+    'dirNode': 'd',
+    'generatorMap': 'g',
+    'rippleState': 'r',
+    'offer': 'o',
+    'ownerDir': 'O',
+    'bookDir': 'B',
+    'contract': 'c',
+    'skipList': 's',
+    'escrow': 'u',
+    'amendment': 'f',
+    'feeSettings': 'e',
+    'ticket': 'T',
+    'signerList': 'S',
+    'paychan': 'x',
+    'check': 'C',
+    'uriToken': 'U',
+    'depositPreauth': 'p',
 }
 
 
@@ -39,26 +39,60 @@ def address_to_hex(address: str) -> str:
     """Convert an address to its hexadecimal representation."""
     # Assuming the address is already in hexadecimal form.
     # If the address is in another format, you will need to convert it to hex.
-    return addresscodec.decode_classic_address(address).hex().upper()
-
+    return addresscodec.decode_classic_address(address).hex()
 
 def ledger_space_hex(name: str) -> str:
     """Get the hexadecimal representation of a ledger space."""
-    return format(ord(ledger_spaces[name]), "x").zfill(4)
+    return format(ord(ledger_spaces[name]), 'x').zfill(4)
+
+
+def hash_offer(address: str, sequence: int) -> str:
+    """Compute the hash of a Offer."""
+    return sha512_half(
+        ledger_space_hex('offer') +
+        address_to_hex(address) +
+        format(sequence, 'x').zfill(BYTE_LENGTH * 2)
+    ).upper()
+
+
+def hash_check(address: str, sequence: int) -> str:
+    """Compute the hash of a Check."""
+    return sha512_half(
+        ledger_space_hex('check') +
+        address_to_hex(address) +
+        format(sequence, 'x').zfill(BYTE_LENGTH * 2)
+    ).upper()
+
+
+def hash_escrow(address: str, sequence: int) -> str:
+    """Compute the hash of a Escrow."""
+    return sha512_half(
+        ledger_space_hex('escrow') +
+        address_to_hex(address) +
+        format(sequence, 'x').zfill(BYTE_LENGTH * 2)
+    ).upper()
 
 
 def hash_payment_channel(address: str, dst_address: str, sequence: int) -> str:
     """Compute the hash of a Payment Channel."""
     return sha512_half(
-        ledger_space_hex("paychan")
-        + address_to_hex(address)
-        + address_to_hex(dst_address)
-        + format(sequence, "x").zfill(BYTE_LENGTH * 2)
-    )
+        ledger_space_hex('paychan') +
+        address_to_hex(address) +
+        address_to_hex(dst_address) +
+        format(sequence, 'x').zfill(BYTE_LENGTH * 2)
+    ).upper()
 
 
-def hash_uri_token(address: str, uri: str) -> str:
-    """Compute the hash of a URI Token."""
+def hash_uri_token(issuer: str, uri: str) -> str:
+    """Compute the hash of a URIToken."""
     return sha512_half(
-        ledger_space_hex("uriToken") + address_to_hex(address) + str_to_hex(uri).upper()
-    )
+        ledger_space_hex('uriToken') +
+        address_to_hex(issuer) +
+        str_to_hex(uri)
+    ).upper()
+
+def hash_hook(hex: str) -> str:
+    """Compute the hash of a Hook."""
+    return sha512_half(
+        hex
+    ).upper()
